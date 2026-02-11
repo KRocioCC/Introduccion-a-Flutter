cbb34b574a86087e80eeddd8794c56e779b58cca
KRocioCC <krociocalderon@gmail.com>
2026-02-11 17:04:55 -0400

    estructura de directorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : Scaffold (
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      )
+      home : CounterScreen(),
     );
   }
   
